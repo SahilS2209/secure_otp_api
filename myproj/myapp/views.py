48e2eb1d4496b1473555dd628758dfac6eb919d2
shimirikanikam <shimirikanikam@gmail.com>
2023-08-07 16:26:32 +0530

    changes in views.py

--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -2,20 +2,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .services import OTPService
 
-request_otp_service = OTPService.request_otp
-verify_otp_service = OTPService.verify_otp
-
 @api_view(['POST'])
 def request_otp_view(request):
     mobile_number = request.data.get('mobile_number')
 
-    if not mobile_number or not mobile_number.isdigit() or len(mobile_number) != 10:
-        return Response({'error': 'Please provide a valid 10-digit mobile number.'}, status=400)
+    if not mobile_number.isdigit() or not (6 <= int(mobile_number[0]) <= 9) or len(mobile_number) != 10:
+        return Response({'error': 'Please provide a valid 10-digit mobile number starting with a digit between 6 and 9.'}, status=400)
 
-    if not 6 <= int(mobile_number[0]) <= 9:
-        return Response({'error': 'Mobile number should start with a digit between 6 and 9.'}, status=400)
-
-    otp, message = request_otp_service(mobile_number)
+    otp, message = OTPService.request_otp(mobile_number)
     if not otp:
         return Response({'error': message}, status=429)
 
@@ -26,16 +20,13 @@ def verify_otp_view(request):
     mobile_number = request.data.get('mobile_number')
     user_otp = request.data.get('otp')
 
-    if not mobile_number or not mobile_number.isdigit() or len(mobile_number) != 10:
+    if not mobile_number.isdigit() or len(mobile_number) != 10:
         return Response({'error': 'Please provide a valid 10-digit mobile number.'}, status=400)
 
-    if not user_otp:
-        return Response({'error': 'Please provide the OTP.'}, status=400)
-
-    if len(user_otp) != 6 or not user_otp.isdigit():
+    if not user_otp.isdigit() or len(user_otp) != 6:
         return Response({'error': 'Please provide a valid 6-digit OTP.'}, status=400)
 
-    is_verified, message = verify_otp_service(mobile_number, user_otp)
+    is_verified, message = OTPService.verify_otp(mobile_number, user_otp)
     if is_verified:
         return Response({'message': message}, status=200)
     else:

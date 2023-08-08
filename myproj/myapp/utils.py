3bb00a4fabeca63ff47349539c231dd0f5dda016
Sandesh-Kanagal <sandeshkanagal2000@gmail.com>
2023-08-08 14:58:35 +0530

    changes in views.py utils.py and added serializers

--- myproj/myapp/utils.py
+++ myproj/myapp/utils.py
@@ -1,9 +1,16 @@
-import re
 from django.utils import timezone
+import re
+from rest_framework.response import Response
 
 def get_current_datetime():
     return timezone.now()
 
-def validate_mobile_number(mobile_number):
-    pattern = r'^[6-9]\d{9}$'
-    return bool(re.match(pattern, mobile_number))
+def mobile_number_pattern_validation(mobile_number):
+        mobile_number_pattern = r'^[6-9]\d{9}$'
+        if not re.match(mobile_number_pattern, mobile_number):
+            return Response({'error': 'Please provide a valid 10-digit mobile number starting with a digit between 6 and 9.'}, status=400)
+
+def otp_number_pattern_validation(user_otp):
+    otp_pattern = r'^\d{6}$'
+    if not re.match(otp_pattern, user_otp):
+        return Response({'error': 'Please provide a valid 6-digit OTP.'}, status=400)

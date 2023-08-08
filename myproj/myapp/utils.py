9e33575e6fd92113a68b68e6f3baa21aede0c3ed
SahilS2209 <shrivastavsahil310@gmail.com>
2023-08-08 14:38:55 +0530

    changes in utils and views

--- myproj/myapp/utils.py
+++ myproj/myapp/utils.py
@@ -1,4 +1,9 @@
+import re
 from django.utils import timezone
 
 def get_current_datetime():
     return timezone.now()
+
+def validate_mobile_number(mobile_number):
+    pattern = r'^[6-9]\d{9}$'
+    return bool(re.match(pattern, mobile_number))
--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -1,15 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .services import OTPService
+import base64
+from .utils import validate_mobile_number
+
 
 @api_view(['POST'])
 def request_otp_view(request):
-    mobile_number = request.data.get('mobile_number')
+    encoded_mobile_number = request.data.get('mobile_number')
 
-    if not mobile_number.isdigit() or not (6 <= int(mobile_number[0]) <= 9) or len(mobile_number) != 10:
+    if not encoded_mobile_number:
+        return Response({'error': 'Please provide the encoded mobile number.'}, status=400)
+
+    try:
+        decoded_mobile_number = base64.b64decode(encoded_mobile_number).decode('utf-8')
+    except:
+        return Response({'error': 'Invalid encoded mobile number.'}, status=400)
+
+    if not validate_mobile_number(decoded_mobile_number):
         return Response({'error': 'Please provide a valid 10-digit mobile number starting with a digit between 6 and 9.'}, status=400)
 
-    otp, message = OTPService.request_otp(mobile_number)
+    otp, message = OTPService.request_otp(decoded_mobile_number)
     if not otp:
         return Response({'error': message}, status=429)
 
@@ -17,16 +28,27 @@ def request_otp_view(request):
 
 @api_view(['POST'])
 def verify_otp_view(request):
-    mobile_number = request.data.get('mobile_number')
+    encoded_mobile_number = request.data.get('mobile_number')
     user_otp = request.data.get('otp')
 
-    if not mobile_number.isdigit() or len(mobile_number) != 10:
-        return Response({'error': 'Please provide a valid 10-digit mobile number.'}, status=400)
+    if not encoded_mobile_number:
+        return Response({'error': 'Please provide the encoded mobile number.'}, status=400)
 
-    if not user_otp.isdigit() or len(user_otp) != 6:
+    try:
+        decoded_mobile_number = base64.b64decode(encoded_mobile_number).decode('utf-8')
+    except:
+        return Response({'error': 'Invalid encoded mobile number.'}, status=400)
+
+    if not validate_mobile_number(decoded_mobile_number):
+        return Response({'error': 'Please provide a valid 10-digit mobile number starting with a digit between 6 and 9.'}, status=400)
+
+    if not user_otp:
+        return Response({'error': 'Please provide the OTP.'}, status=400)
+
+    if len(str(user_otp)) != 6 or not str(user_otp).isdigit():
         return Response({'error': 'Please provide a valid 6-digit OTP.'}, status=400)
 
-    is_verified, message = OTPService.verify_otp(mobile_number, user_otp)
+    is_verified, message = OTPService.verify_otp(decoded_mobile_number, user_otp)
     if is_verified:
         return Response({'message': message}, status=200)
     else:
